616010ea973970920894745534d30e2c3b16aa89
Fabian Grandinetti <grandinetti00@outlook.com>
2023-04-16 16:27:59 -0300

    start of project

---
+++ im-phone/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div>
+        <img src="/logo.png" className="logo"/>
+      </div>
+      <h1>I('m)Phone</h1>
+      <div className="subtitulo">
+        <p>
+          CHOOSE YOUR NEW INPHONE
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
