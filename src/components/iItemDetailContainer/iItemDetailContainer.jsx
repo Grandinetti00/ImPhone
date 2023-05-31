92ccbb60eb91b315a35e177ae6a14ef0c8a29cea
Fabian Grandinetti <grandinetti00@outlook.com>
2023-05-30 22:11:49 -0300

    add firebase

--- src/components/iItemDetailContainer/iItemDetailContainer.jsx
+++ src/components/iItemDetailContainer/iItemDetailContainer.jsx
@@ -1,24 +1,11 @@
-import { useEffect, useState } from "react";
-import {getData} from "../../helpers/getData";
-import { parsePath, useParams } from "react-router-dom";
+import { useItem } from "../../hooks/useItem"
+import { useParams } from "react-router-dom"
 import ItemDetail from "../ItemDetail/ItemDetail";
 import sticker from '../../assets/img/sticker.webp';
 
 const ItemDetailContainer = () => {
-    const [item, setItem] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const {itemId} = useParams();
-    console.log(itemId)
-    console.log(item)
-
-    useEffect(() => {
-        setLoading(true);
-
-        getData()
-            .then((data) => setItem(data.find((el) => el.id === Number(itemId))))
-            .catch(err => console.log(err))
-            .finally(() => setLoading(false))
-    }, [])
+    const {itemId} = useParams()
+    const {loading, item} = useItem(itemId)
 
     return (
         <div>
