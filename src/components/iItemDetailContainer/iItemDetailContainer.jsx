df8770b69cb89d87b23f9ed04b54660d3ae38470
Fabian Grandinetti <grandinetti00@outlook.com>
2023-06-01 09:58:52 -0300

    change in itemDetail css

--- src/components/iItemDetailContainer/iItemDetailContainer.jsx
+++ src/components/iItemDetailContainer/iItemDetailContainer.jsx
@@ -2,13 +2,14 @@ import { useItem } from "../../hooks/useItem"
 import { useParams } from "react-router-dom"
 import ItemDetail from "../ItemDetail/ItemDetail";
 import sticker from '../../assets/img/sticker.webp';
+import './itemDetailContainer.css';
 
 const ItemDetailContainer = () => {
     const {itemId} = useParams()
     const {loading, item} = useItem(itemId)
 
     return (
-        <div>
+        <div className="datilContainer">
             {
                 loading
                     ? <img className='loading' src={sticker} alt='loading' />
